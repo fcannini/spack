ee874c702ee1f87f0d1ab41468d8c30b68b7ce2c
Fabricio Cannini <fabricio@versatushpc.com.br>
2019-07-15 21:05:16 -0300

    py-thermotools: New package. Dependency of PKG-4.

---
+++ var/spack/repos/vhpc/packages/py-thermotools/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class PyThermotools(PythonPackage):
+    """
+    A lowlevel implementation of (transition-based and histogram) reweighting analyis methods.
+    """
+
+    homepage = "https://github.com/markovmodel/thermotools"
+    url      = homepage + "/archive/0.2.7.tar.gz"
+
+    version('0.2.7', sha256='171c0142565ef75aedf20635605846637588de098268f2c58080dbee32c2f0b9')
+
+    depends_on('py-setuptools', type='build')
+    depends_on('py-cython@0.20:', type='build')
+    depends_on('py-numpy@1.7:')
+    depends_on('py-scipy@0.11:')
+    depends_on('py-msmtools@1.1.3:')
+
+    extends('python', type=('build', 'link', 'run'))
+
