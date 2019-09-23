6b14084c525ad23f8b0f1db0ab3429bb0cb04d68
Fabricio Cannini <fabricio@versatushpc.com.br>
2019-09-23 14:37:26 -0300

    quantum-espresso: Add new package versions

--- var/spack/repos/vhpc/packages/quantum-espresso/package.py
+++ var/spack/repos/vhpc/packages/quantum-espresso/package.py
@@ -20,6 +20,9 @@ class QuantumEspresso(Package):
     url = 'https://gitlab.com/QEF/q-e/-/archive/qe-6.3/q-e-qe-6.3.tar.gz'
     git = 'https://gitlab.com/QEF/q-e.git'
 
+    version('6.4.1',  sha256='b0d7e9f617b848753ad923d8c6ca5490d5d82495f82b032b71a0ff2f2e9cfa08')
+    version('6.4',    sha256='781366d03da75516fdcf9100a1caadb26ccdd1dedd942a6f8595ff0edca74bfe')
+    version('6.3MaX', sha256='9e130a8042b244964f4b7b04ef3a694f610610d166ae809c4a045aa603633688')
     version('6.3',   '1b67687d90d1d16781d566d44d14634c')
     version('6.2.1', '769cc973382156bffd35254c3dbaf453')
     version('6.2.0', '972176a58d16ae8cf0c9a308479e2b97')
