e7cc71dd0baccb7aa18b7c7131df7c9823a49aa9
Fabricio Cannini <fabricio@versatushpc.com.br>
2019-09-26 21:53:00 -0300

    taxila-lbm: Recipe ready.

--- var/spack/repos/vhpc/packages/taxila-lbm/package.py
+++ var/spack/repos/vhpc/packages/taxila-lbm/package.py
@@ -6,7 +6,7 @@
 from spack import *
 
 
-class TaxilaLbm(Package):
+class TaxilaLbm(MakefilePackage):
     """
     a parallel implementation of the Lattice Boltzmann Method
     for simulation of flow in porous and geometrically complex media.
@@ -16,10 +16,37 @@ class TaxilaLbm(Package):
     url      = homepage
     git      = url + '.git'
 
-    version('master', branch='master')
+    version('7402aff', commit='7402aff')
 
-    depends_on('petsc@:3.6.4 +debug ~superlu-dist')
-    depends_on('py-petsc4py@3.7.0')
+    depends_on('automake', type='build')
+    depends_on('python')
+    depends_on('petsc@:3.6.4 ~superlu-dist')
+    depends_on('py-petsc4py@3.6.0')
+    depends_on('py-numpy@:1.16.99', when='^python@:2.8')
+    depends_on('mpi', type=('build', 'link', 'run'))
+
+    patch('geometry.patch',
+          sha256='9b1bee4bcc7b477a4943a8038bb1d879e51d2588e8da7aa55c603c36b48fb8b4')
+    patch('shlib.patch',
+          sha256='38ed0a1130a5751a16b685f3ad19c7954e833ee237e1b30d105bb9a92a4f796a')
+
+    parallel = False
+
+    def setup_environment(self, spack_env, run_env):
+        spack_env.set('TAXILA_DIR', self.stage.source_path)
+
+    def build(self, spec, prefix):
+        with working_dir('src/lbm/'):
+            make()
+
+        with working_dir('src/geometry/'):
+            make()
+
     def install(self, spec, prefix):
-        make()
-        make('install')
+        mkdirp(prefix.bin, prefix.lib, prefix.include)
+
+        install('lib/lbm.a', prefix.lib)
+        install('lib/liblbm.so', prefix.lib)
+        install('src/geometry/createGeom2DPetsc', prefix.bin)
+        install('src/geometry/createGeom3DPetsc', prefix.bin)
+        install('include/lbm_definitions.h', prefix.include) 
